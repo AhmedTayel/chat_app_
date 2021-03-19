35ead8b8372cd59c20a9c9baa0189e9fd9ef1db1
Ahmed Tayel <ahmed.tayel97@gmail.com>
2021-03-20 00:45:30 +0200

    Fix to generate token on room creation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,10 +1,10 @@
 class Room < ApplicationRecord
   has_many :chats 
+  before_create :generate_token
   after_create :start_chat_count
-
+  
   def as_json(options={})
     {
-      
       :token => token,
       :name => name,
       :chat_count => chat_count,
@@ -16,5 +16,8 @@ class Room < ApplicationRecord
   private
   def start_chat_count
     self.update(chat_count: 0)
+  end
+  def generate_token
+    self.token = RandomToken.genf(5)
   end
 end
