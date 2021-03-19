b6196259d89eac8ed49272fcf0ac94665c2cd9ae
Ahmed Tayel <ahmed.tayel97@gmail.com>
2021-03-19 23:01:58 +0200

    Setting up nested routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+
+  resources :rooms, param: :token, only: [:index, :show, :create] do
+   resources :chats, param: :number, only: [:index, :show, :create] do 
+    resources :messages, param: :message_number, only: [:index, :show, :create]
+   end
+  end
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
