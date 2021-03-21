e675f5a2bde221be58d4017c6f778c42a996a7c5
Ahmed Tayel <ahmed.tayel97@gmail.com>
2021-03-21 20:58:35 +0200

    Configure CORS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3001','http://127.0.0.1:5500']
+  
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
